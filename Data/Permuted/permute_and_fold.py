58f2293ac4516b0c490b14c2a057542911326a13
fu19841 <fu19841@bristol.ac.uk>
2022-06-23 16:33:18 +0100

    Initial commit

---
+++ Data/Permuted/permute_and_fold.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import os
+from numpy.random import choice
+from sklearn.model_selection import KFold
+from string import ascii_uppercase
+
+if __name__ == '__main__':
+
+    datasets = ['UMLS-43']#[x.split('.')[0] for x in os.listdir('../raw_edgelists')]
+    knockout_proportion = 0.05
+
+    for dataset in datasets:
+
+        remaining_edgelist = pd.read_csv(f'../raw_edgelists/{dataset}.tsv', sep='\t', header=None)
+        remaining_proportion = 1 + knockout_proportion # + proportion to include full graph in below loop
+
+        num_to_knockout = round(knockout_proportion * len(remaining_edgelist))
+        knockouts = pd.DataFrame()
+
+
+        while remaining_proportion > knockout_proportion:
+            remaining_proportion = round(remaining_proportion - knockout_proportion, 2)
+
+            # Perform knockouts
+            if remaining_proportion < 1:
+                knockout_IDs = choice(remaining_edgelist.index, num_to_knockout, replace=False)
+                knocked_edges = remaining_edgelist.loc[knockout_IDs]
+                knockouts = knockouts.append(knocked_edges)
+                remaining_edgelist.drop(knockout_IDs, inplace=True)
+                remaining_edgelist.reset_index(inplace=True, drop=True)
+
+            # Save data
+            new_edgelist_name = f'{dataset}_{remaining_proportion}'
+            sub_dir = f'{dataset}/{new_edgelist_name}'
+            if not os.path.exists(sub_dir):
+                os.mkdir(sub_dir)
+            remaining_edgelist.to_csv(f'{sub_dir}/{new_edgelist_name}.tsv', index=False, header=None, sep='\t')
+            knockouts.to_csv(f'{sub_dir}/knockouts_{new_edgelist_name}.tsv', index=False, header=None, sep='\t')
+
+            # Create 5 folds for cross validation
+            splits = list(KFold(5).split(remaining_edgelist.index))
+            for i, split in enumerate(splits):
+                fold_label = ascii_uppercase[i]
+                
+                # Randomly divide the 20% split into 10%/10% test/val
+                test_inds = choice(split[1], int(len(split[1])/2), replace=False)
+                val_inds = [ind for ind in split[1] if ind not in test_inds]
+                
+                # Locate edges
+                train = remaining_edgelist.loc[split[0]]
+                test = remaining_edgelist.loc[test_inds]
+                val = remaining_edgelist.loc[val_inds]
+
+                # Save
+                fold_dir = sub_dir + f'/{new_edgelist_name}_{fold_label}'
+                if not os.path.exists(fold_dir):
+                    os.mkdir(fold_dir)
+                train.to_csv(f'{fold_dir}/train.txt', sep='\t', index=False, header=None)
+                test.to_csv(f'{fold_dir}/test.txt', sep='\t', index=False, header=None)
+                val.to_csv(f'{fold_dir}/valid.txt', sep='\t', index=False, header=None)
+
+
+            
---
+++ README.md
@@ -0,0 +1,5 @@
+# Knowledge Graph Embedding Hyperparameter Sensitivity
+
+This project aims to build on the findings of previous work [link to preprint here when uploaded] that showed that the response of embedding quality to changes in hyperparameter values is highly variable by dataset. We will use a similar methodology as the previous study, but this time we will include dataset statistics in our sensitivity model by iteratively permuting each of the three KGs to create a range of graph characteristics. Grid-search configurations will also be altered and more carefully designed than they were previously. By performing this more thorough experiment, we hope to identify areas of consistency across KGEs' hyperparameter sensitivities, as well as interactions between them and the measured graph statistics.
+
+Contact: oliver.lloyd@bristol.ac.uk
